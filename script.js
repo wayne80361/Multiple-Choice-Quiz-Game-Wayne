b5680289e8fb3c27faa55f2baeb7624bed1ffe09
Wayne <wayne80361@gmail.com>
2023-08-29 23:39:15 -0400

    add more too pseudo code time and game over. In HTML, add one more section to check correct/wrong answer, and its CSS. Add some global variable in javascript.

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // DEPENDCIES =========================================
 var question = document.getElementById("question-line");
+var answer = document.getElementsByClassName("option-button-container");
+var checkcorrect = document.getElementsByClassName("checker");
+var time = document.getElementById("Time-Left");
+var highscore = document.getElementById("view-highscore");
+
+console.log(checkcorrect);
 
 // DATA ==============================================
 // question sets
@@ -7,14 +13,20 @@ var question = document.getElementById("question-line");
 // FUNCTION ==========================================
 
 // USER INTERACTIONS ==================================
-// when user click start,
-// one question will shown
-// with 4 choices
-// if select the correct one,
-// go to next question
-// if wrong,
-// subtract time to time-left
-// go to next question
+//* when user click start,
+//-- one question will shown
+//-- and timer will start to countdown
+//-- with 4 choices
+//* if select the correct one,
+// --- go to next question
+//* if wrong,
+// --- subtract time to time-left
+// ---go to next question
+//* game is over/finish
+//  -- when time is over
+//  -- or answer all question
+//* shown score
+//  -- with a name input box
 
 // INITIALIZATION ======================================
 // start quiz
