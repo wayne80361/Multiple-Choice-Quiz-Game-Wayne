b42f9d798c4a5b2ff004d9a076e937fd7a2eb848
Wayne <wayne80361@gmail.com>
2023-08-29 23:19:00 -0400

    add Pseudo code to javascript.

---
+++ script.js
@@ -0,0 +1,20 @@
+// DEPENDCIES =========================================
+var question = document.getElementById("question-line");
+
+// DATA ==============================================
+// question sets
+
+// FUNCTION ==========================================
+
+// USER INTERACTIONS ==================================
+// when user click start,
+// one question will shown
+// with 4 choices
+// if select the correct one,
+// go to next question
+// if wrong,
+// subtract time to time-left
+// go to next question
+
+// INITIALIZATION ======================================
+// start quiz
